7ecfe5fe0f9c9e7c00e780b25604d12eeec64506
Fabio <fabio_almeida89@outlook.com>
2025-10-17 18:53:02 -0300

    feat(hackerrank): Realizando desafio data_types, usei variaveis inteiras, double e strings

---
+++ hackerrank/Tutorials/30-days-of-code/Day_1-Data Types/data_types.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare as segundas variáveis ​​inteiras, double e strings.
+segundo_i = int(input())
+segundo_double = float(input())
+segunda_string = str(input())
+
+# Leia e salve uma variável inteira, double e string em suas variáveis.
+
+# Imprima a soma das duas variáveis ​​inteiras em uma nova linha.
+print(i + segundo_i)
+
+# Imprima a soma das variáveis double em uma nova linha.
+print(d + segundo_double)
+
+# Concatene e imprima as variáveis ​​strings em uma nova linha
+print(s + segunda_string)
+
+# A variável 's' acima deve ser impressa primeiro.
